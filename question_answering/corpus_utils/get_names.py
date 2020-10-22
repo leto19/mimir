8c322cead2e5e9ff71d97e890869c606f6817ce6
George Close <georgeclose14@gmail.com>
2020-10-22 16:15:32 +0100

    change how get_names reads the input file, improving results

--- question_answering/corpus_utils/get_names.py
+++ question_answering/corpus_utils/get_names.py
@@ -1,18 +1,21 @@
 import nltk, sys 
 
 
-
 def download_models():
     nltk.download('punkt')
     nltk.download('averaged_perceptron_tagger')
     nltk.download('maxent_ne_chunker')
     nltk.download('words')
-def get_tokens_from_text(file):
 
+def get_line_list_from_file(file):
     with open(file) as f:
-        file_lines = f.read()
+        line_list = f.readlines()
+    
+    return line_list
 
-    tokens = nltk.word_tokenize(file_lines)
+
+def get_tokens_from_text(line):
+    tokens = nltk.word_tokenize(line)
     
     return tokens
 
@@ -21,17 +24,20 @@ def get_named_entities(tokens):
     return entities
 
 download_models()
-t = get_tokens_from_text(sys.argv[1])
-en = get_named_entities(t)
 
+line_list = get_line_list_from_file(sys.argv[1])
+print(line_list)
 entity_list = list()
+for line in line_list:
+    print(line)
+    line.strip("\n")
+    t = get_tokens_from_text(line)
+    entities = get_named_entities(t)
 
-for l in en:
-    if len(l) == 1:
-        #print(l.label())
-        #print(l[0][1])
-        if "%s (%s)" % ( l[0][0], l.label() ) not in entity_list:
-            entity_list.append("%s (%s)" % ( l[0][0], l.label() ) )
+    for l in entities:
+        if len(l) == 1: #it's an entity
+            if "%s (%s)" % ( l[0][0], l.label().lower() ) not in entity_list: #if it's not in the list
+                entity_list.append("%s (%s)" % ( l[0][0], l.label().lower() ) )
 
 
 print(entity_list)

ce72e726ae934834db874a916f5b53537ef78602
acidrobin <jclayton558@gmail.com>
2020-10-23 18:06:58 +0800

    Minor changes to sparknotes_book_list.py

--- question_answering/corpus_utils/sparknotes_book_list.py
+++ question_answering/corpus_utils/sparknotes_book_list.py
@@ -1,4 +1,4 @@
-import csv 
+from utils import csv_to_list, levenshtein
 import os
 import os.path as op
 import re
@@ -13,21 +13,11 @@ title_regex = re.compile(r'__card__title__link--full-card-link"\s*href="\S*">(.*
 spark_books = re.findall(title_regex, lit_page_text)
 
 if len(sys.argv) == 1:
-	NarrativeQADir = "/home/jonathan/Desktop/corpora/narrativeqa-master"
+	NarrativeQADir = os.environ["NARRATIVEQA_DIR"] #Set this environment variable for convenience
 else:
 	NarrativeQADir = sys.argv[1]
+
 docs_file_path = op.join(NarrativeQADir, "documents.csv")
-
-def csv_to_list(csv_file_path):
-	line_list = []
-	
-	with open(csv_file_path, newline="") as csvfile:
-		csvreader = csv.reader(csvfile)
-		for row in csvreader:
-			line_list.append(row)
-
-	return(line_list)
-
 
 docs_list = csv_to_list(docs_file_path)
 nqa_books = []
@@ -39,11 +29,22 @@ for line in docs_list:
 	except:
 		pass
 
+for i in range(len(nqa_books)):
+	if nqa_books[i].endswith(" (novel)"):
+		nqa_books[i] = nqa_books[i][:-8]
 
 both_books = set(nqa_books).intersection(set(spark_books))
+
 
 print("N. books in NarrativeQA: {}".format(len(nqa_books)))
 print("N. books on SparkNotes: {}".format(len(spark_books)))
 print("N. books in both: {}".format(len(both_books)))
 print("List of titles:")
 print(both_books)
+
+
+def check_for_missed_titles():
+	for book in nqa_books:
+		if book not in both_books:
+			print("{} - {}".format(book, min(spark_books, key = lambda x: levenshtein(x, book))))
+			input("Press enter to check next")

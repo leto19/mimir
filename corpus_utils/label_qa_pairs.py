c3c04298d672cfe9d11751b0aa7c8a65f80390b9
acidrobin <jclayton558@gmail.com>
2020-11-24 18:46:36 +0800

    Tidied up some filepath issues

--- corpus_utils/label_qa_pairs.py
+++ corpus_utils/label_qa_pairs.py
@@ -5,24 +5,29 @@ import os.path as op
 import pandas as pd
 #import pandastable    # Use these for ease of viewing labels so far
 #import tkinter as tk
+import time
 import re
 from utils import csv_to_list, load_or_create_object
 import sys
 
+
 if len(sys.argv) == 1:
-	MimirDir = os.environ["MIMIR_DIR"] #Set to Mimir root directory
-	NarrativeQADir = os.environ["NARRATIVEQA_DIR"] #Set to NarrativeQA root directory
-elif len(sys.argv) == 3:
-	MimirDir = sys.argv[1]
-	NarrativeQADir = sys.argv[2]
+	mimir_dir = os.environ["MIMIR_DIR"] #Set to Mimir root directory
+elif len(sys.argv) == 2:
+	mimir_dir = sys.argv[1]
 
-save_directory = ""
+labels_dir = op.join(mimir_dir, "data", "saved_labels")
 
-qaps_line_list = csv_to_list(op.join(NarrativeQADir, "qaps.csv"))
+if not op.exists(labels_dir):
+	os.mkdir(labels_dir)
+
+my_name = input("Input your name for the labels file (e.g. 'jon'):")
+
+qaps_line_list = csv_to_list(op.join(mimir_dir, "data", "narrativeqa_qas.csv"))
 header = qaps_line_list[0]
-header.append("answer type")
+header.append("answer type") 
 answer_lines = qaps_line_list[1:]
-labels_list_name = op.join(save_directory,"labels_list_tmp.npy")
+labels_list_name = op.join(labels_dir,"labels_list_" + my_name + ".npy") #Here is where we save the labels
 labels_list = load_or_create_object(labels_list_name, [None] * len(answer_lines))
 
 continue_index = 0
@@ -44,7 +49,7 @@ start_input = input("Answer category labeller.\n\
 if start_input == "s":
 	i = continue_index
 elif re.match(r"[0-9]+", start_input):
-	i = start_input
+	i = int(start_input)
 elif start_input == "v":
 	import pandastable
 	import tkinter as tk
@@ -65,12 +70,16 @@ my_input = ""
 
 while (my_input != "q") and (i < len(answer_lines)-1):
 	line = answer_lines[i]
+	if line[1] != "train":
+		print("Question {0} not in train set, skipping".format(i + 1))
+		i+=1
+		continue
 	print("Question {0} of {1}:".format(i + 1, len(answer_lines)))
 	print(line[2])
 	print("Answers:")
-	print(line[3],line[4])
+	print(line[3], "|", line[4])
 	my_input = input("Select answer type:\n\
-[1] person [2] organization [3] location [4] personal description [5] reason \n\ 
+[1] person [2] organization [3] location [4] personal description [5] reason \n\
 [6] event [7] yes-no question [8] co-reference [9] other \n\
 [b] back [n] next [q] quit ")
 	if my_input in "123456789":
@@ -83,4 +92,7 @@ while (my_input != "q") and (i < len(answer_lines)-1):
 		i -= 1
 	elif my_input == "q":
 		exit(1)
+	else:
+		print("Invalid input")
+		time.sleep(1)
 	os.system("clear")

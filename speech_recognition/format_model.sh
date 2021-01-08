a715408b1201017babd807473353b78da92a5f93
George Close <georgeclose14@gmail.com>
2021-01-08 11:20:19 +0000

    cleanup

--- speech_recognition/format_model.sh
+++
@@ -1,5 +0,0 @@
-mv $1/ivector_extractor $1/ivector/
-mv $1/graph/HCLG.fst $1/HCLG.fst
-cp $1/conf/splice.conf $1/ivector/splice.conf
-mv $1/conf/mfcc.conf $1/mfcc.conf
-cp $1/lang/words.txt $1/words.txt

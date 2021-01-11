ab91a0eafd5ab429b00b687e5ec5645fa8ccd49a
George Close <georgeclose14@gmail.com>
2021-01-11 14:05:53 +0000

    updated installer script to reflect new pip module requirements

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pip install wikipedia vosk pyaudio pydub spacy gtts
+pip install wikipedia vosk pyaudio pydub spacy gtts scipy noisereduce
 python -m spacy download en_core_web_sm #Download spaCy model
 MIMIR_DIR=$(pwd)"/"
 echo $MIMIR_DIR

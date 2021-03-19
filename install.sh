b4ac9048b71a423602d6ba6374ce660f64ec7f85
kgreed1 <kgreed1@sheffield.ac.uk>
2021-03-19 12:19:17 +0000

    added transformers to requirements

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pip install wikipedia vosk pyaudio pydub spacy gtts scipy noisereduce wave 
+pip install wikipedia vosk pyaudio pydub spacy gtts scipy noisereduce wave transformers
 python -m spacy download en_core_web_sm #Download spaCy model
 
 

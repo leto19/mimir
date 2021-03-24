f2d8c2cf2a599fe41cb8c4f6095e89eaf3723206
George Close <glclose1@sheffield.ac.uk>
2021-03-24 14:39:46 +0000

    added audio effects to tts output

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pip install wikipedia vosk pyaudio pydub spacy gtts scipy wave torch 
+pip install wikipedia vosk pyaudio pydub spacy gtts scipy wave torch pysndfx
 transformers
 python -m spacy download en_core_web_sm #Download spaCy model
 
--- tts/gtts_run.py
+++ tts/gtts_run.py
@@ -4,7 +4,7 @@ from pydub.playback import play
 import os
 from ctypes import *
 from contextlib import contextmanager
-
+import soundfile as sf
 ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
 
 def py_error_handler(filename, line, function, err, fmt):
@@ -20,14 +20,50 @@ def noalsaerr():
   yield
   asound.snd_lib_error_set_handler(None)
 
+def add_effect(in_file,out_file):
+  from pysndfx import AudioEffectsChain
+
+  fx = (
+      AudioEffectsChain()
+      #.highshelf()
+      .phaser()
+      #.delay(0.4,0.6,[0.5,0.25])
+      #.overdrive(gain=5,colour=4)
+      .pitch(-300,use_tree=True)
+      .reverb(reverberance=5)
+      .speed(1.1)
+      #.bend([["0","180",".25"],["0","90","1"],["0","10",".25"]])
+      .lowshelf(gain=-20.0,frequency=250,slope=0.8)
+      .tremolo(90,50)
+      .equalizer(20)
+      .custom("chorus 1 .5 100 0.8 1 3 -s")
+      .custom("synth sine fmod 10 echo 0.8 0.8 29 0.8")
+  )
+
+  fx(in_file,out_file)
+  #y,fs = sf.read(in_file)
+  #print(type(y))
+  
 def tts(text):
   with noalsaerr():
     language = 'en'
-    yobj = gTTS(text=text, lang=language, slow=False)
+    yobj = gTTS(text=text,tld="co.uk", lang=language, slow=False)
     yobj.save("response.mp3")
+    
+
+
     res = AudioSegment.from_mp3("response.mp3")
-    play(res)
+    res.export("response.wav",format="wav")
     os.remove("response.mp3")
+    add_effect("response.wav","response_e.wav")
+    res = AudioSegment.from_file("response_e.wav")
+    os.remove("response.wav")
+    os.remove("response_e.wav")
+    play(res)
+
+
+
+
 
 if __name__ == "__main__":
   import sys
